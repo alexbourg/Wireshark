9afde601fa36c4ed65a609d87a681a83e31e9fb0
Alex Bourg <51832082+alexbourg@users.noreply.github.com>
2021-05-22 16:34:54 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
-.\src\Wireshark-win64-3.4.5.exe /S
-.\src\npcap-1.31_Silent.exe /exenoui /qn
+cd "%~dp0"
+start /w .\src\Wireshark-win64-3.4.5.exe /S
+start /w .\src\npcap-1.31_Silent.exe /exenoui /qn
